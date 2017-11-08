1106fc439cbff2e0110a1306519e7a0aba5ec2e9
JimmyLin39 <jimmylin0309@gmail.com>
2017-11-08 01:27:59 +0000

    Create a URL Submission Form

--- express_server.js
+++ express_server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 var urlDatabase = {
@@ -21,15 +22,37 @@ app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// passing urlDatabase data to .views/urls_index.ejs
+// list short form link and it's full url
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// Create a URL Submission Form
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  // debug statement to see POST parameters
+  console.log(req.body);
+  // Respond with 'Ok' (we will replace this)
+  res.send("Ok");
+});
+
+// passing request data to .view/urls_show.ejs
+// list full url by request short form
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id };
+  let templateVars = {
+    shortURL: req.params.id,
+    urls: urlDatabase
+  };
   res.render("urls_show", templateVars);
 });
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
